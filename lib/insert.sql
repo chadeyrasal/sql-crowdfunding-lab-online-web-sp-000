6fc09a1b6805f4cc8725da1da0ab70d8504439df
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 15:09:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,7 +3,7 @@ INSERT INTO users (name, age) VALUES ('Bear', 6);
 INSERT INTO users (name, age) VALUES ('Iguana', 4);
 INSERT INTO users (name, age) VALUES ('Alex', 33);
 INSERT INTO users (name, age) VALUES ('Finnebar', 17);
-INSERT INTO users (name, age) VALUES ('Finnebar', 17);
+INSERT INTO users (name, age) VALUES ('Sophie', 24);
 INSERT INTO users (name, age) VALUES ('Rosey', 9);
 INSERT INTO users (name, age) VALUES ('Victoria', 23);
 INSERT INTO users (name, age) VALUES ('Franz', 100);
@@ -19,4 +19,4 @@ INSERT INTO users (name, age) VALUES ('Pacha', 5);
 INSERT INTO users (name, age) VALUES ('Ena', 24);
 INSERT INTO users (name, age) VALUES ('Katie', 24);
 
-(5, 'Amanda', 24), (6, 'Sophie', 24), (7, )
+(5, 'Amanda', 24), (6, ), (7, )
