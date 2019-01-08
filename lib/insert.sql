23a26ba9b3241e0808f38f576b8874a687c79027
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 15:07:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,7 +11,7 @@ INSERT INTO users (name, age) VALUES ('Finnebar', 17);
 INSERT INTO users (name, age) VALUES ('Finnebar', 17);
 INSERT INTO users (name, age) VALUES ('Finnebar', 17);
 INSERT INTO users (name, age) VALUES ('Finnebar', 17);
-INSERT INTO users (name, age) VALUES ('Finnebar', 17);
+INSERT INTO users (name, age) VALUES ('Sirius', 36);
 INSERT INTO users (name, age) VALUES ('Albus', 113);
 INSERT INTO users (name, age) VALUES ('Squid', 5);
 INSERT INTO users (name, age) VALUES ('Whale', 6);
@@ -20,4 +20,4 @@ INSERT INTO users (name, age) VALUES ('Ena', 24);
 INSERT INTO users (name, age) VALUES ('Katie', 24);
 
 (5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
-(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, ), (16, ),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, ), (15, ), (16, ),
