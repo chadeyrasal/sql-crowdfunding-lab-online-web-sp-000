3c7312e9973c6d30f2d2c3dea84e543988d21078
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 15:13:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,7 +24,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
+  VALUES ('The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ('Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30');
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
@@ -41,7 +41,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ('Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
 (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
-(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(3, ),
 (4, ),
 (5, ),
 (6, ),
