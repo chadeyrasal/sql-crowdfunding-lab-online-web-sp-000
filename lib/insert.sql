ec6793b25d2ebae685ef42df0175e5ac5cd8570e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 15:12:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -40,15 +40,9 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
   VALUES ('Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
-
-
-
 (2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
 (3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
 (4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
 (5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
 (6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
 (7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
-(8, ),
-(9, ),
-(10, );
